807a9c9eef2a23d38bbc20bb96d9aa418a200afc
dlftls38 <dlftls38@naver.com>
2020-04-29 03:56:36 +0900

    initial run success!

--- src/main/java/edu/hanyang/submit/TinySEExternalSort.java
+++ src/main/java/edu/hanyang/submit/TinySEExternalSort.java
@@ -1,12 +1,10 @@
 package edu.hanyang.submit;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 import edu.hanyang.indexer.ExternalSort;
+import edu.hanyang.utils.DiskIO;
 import org.apache.commons.lang3.tuple.MutableTriple;
 
 public class TinySEExternalSort implements ExternalSort {
@@ -14,7 +12,7 @@ public class TinySEExternalSort implements ExternalSort {
     public static void main(String[] args)  throws IOException{
         String input_path = "D:/github/informationRetrieval/TinySE-submit/target/test-classes/test.data";
 
-        System.out.println("This is input data");
+        System.out.println("----------test.data----------");
         DataInputStream dataInputStream = new DataInputStream(
                 new BufferedInputStream(
                         new FileInputStream(input_path), 1024)
@@ -35,7 +33,7 @@ public class TinySEExternalSort implements ExternalSort {
 
         String sorted_path = "D:/github/informationRetrieval/TinySE-submit/tmp/sorted.data";
 
-        System.out.println("----------sorted data----------");
+        System.out.println("----------sorted.data----------");
         DataInputStream dataInputStream2 = new DataInputStream(
                 new BufferedInputStream(
                         new FileInputStream(sorted_path), 1024)
@@ -52,42 +50,119 @@ public class TinySEExternalSort implements ExternalSort {
             System.out.println(")");
         }
         dataInputStream2.close();
+
+
+        String run_path = "D:/github/informationRetrieval/TinySE-submit/tmp/run/0.data";
+
+        System.out.println("----------/run/0.data----------");
+        DataInputStream dataInputStream3 = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(run_path), 1024)
+        );
+        for(int i=0;i<15;i++){
+            System.out.print(i + ": (");
+            for(int j=0;j<3;j++){
+                int data = dataInputStream3.readInt();
+                System.out.print(data);
+                if(j!=2){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println(")");
+        }
+        dataInputStream3.close();
     }
 
     public void sort(String infile, String outfile, String tmpDir, int blocksize, int nblocks) throws IOException {
+
         // 1) initial phase
-        // 메모리에 올릴 수 있는 전체 사이즈 = blocksize * nblocks(M) / Integer.SIZE
+
+        // 메모리에 올릴 수 있는 전체 사이즈 = blocksize * nblocks (= M) / Integer.SIZE
         int nElement = blocksize * nblocks / Integer.SIZE;
-        // MutableTriple을 이용해 각 data를 tuple에 담을 것
-        ArrayList<MutableTriple<Integer, Integer, Integer>> dataArr = new ArrayList<>(nElement);
-        // Disk로부터 Data를 읽을 cursor
-        DataInputStream is = new DataInputStream(
-                new BufferedInputStream(
-                        new FileInputStream(infile), blocksize)
-        );
 
-
-
-
-        File file = new File(tmpDir);
-        if (!file.exists()) {
-            file.mkdir();
+        //tmp 폴더 생성
+        File fileTmpDir = new File(tmpDir);
+        if(!fileTmpDir.exists()){
+            fileTmpDir.mkdir();
         }
 
-        DataOutputStream os = new DataOutputStream(
-                new BufferedOutputStream(
-                        new FileOutputStream(outfile), blocksize)
-        );
-        for (int i=0; i<15; i++){
-            for (int j=0; j<3; j++){
+        //test용 sorted.data 생성
+        File sorted = new File(outfile);
+        sorted.createNewFile();
+
+        //test용 sorted.data 채우기
+        ArrayList<MutableTriple<Integer, Integer, Integer>> tmpdataArr = new ArrayList<>(45);
+        DataManager tmpdm = new DataManager();
+        tmpdm.openDis(infile, blocksize);
+        tmpdm.openDos(outfile, blocksize);
+        for(int i=0; i<15; i++){
+            for(int j=0; j<3; j++){
                 // 테스트로 데이터 넣어보기, tmp Directory와 sorted.data 잘 생성됨
-                os.writeInt((i + j + 1)*i*j%30);
+                MutableTriple<Integer, Integer, Integer> tmptuple = new MutableTriple<>();
+                tmpdm.readNext();
+                tmpdm.getTuple(tmptuple);
+                tmpdataArr.add(tmptuple);
+                tmpdm.writeNext(tmpdataArr.get(i*3+j));
             }
         }
+        tmpdm.closeDis();
+        tmpdm.closeDos();
 
+
+
+
+
+        //initial run의 개수
+        int number_of_initial_run=0;
+        //File I/O와 같은 것들을 처리할 DataManager 선언
+        DataManager dm = new DataManager();
+        dm.openDis(infile, blocksize);
+
+        int before = dm.dis.available();
+        //정렬해야하는 input data를 읽을 수 있을 때까지 읽음
+        while(dm.dis.available()!=0){
+            // MutableTriple을 이용해 각 data를 tuple에 담을 것
+            ArrayList<MutableTriple<Integer, Integer, Integer>> dataArr = new ArrayList<>(nElement);
+
+            //사용 할 수 있는 메모리 크기에 따른, 메모리에 올릴 수 있는 element의 수 = nElement 만큼 데이터를 읽어 들임
+            for(int i=0; i<nElement; i++){
+                try{
+                    MutableTriple<Integer, Integer, Integer> tuple = new MutableTriple<>();
+                    dm.readNext();
+                    dm.getTuple(tuple);
+                    dataArr.add(tuple);
+                }catch (EOFException e) {
+                    break;
+                }
+            }
+            dataArr.sort(null);
+
+            String runDir = tmpDir+File.separator+"run";
+            File fileRunDir = new File(runDir);
+            if(!fileRunDir.exists()){
+                fileRunDir.mkdir();
+            }
+
+            String initial_run = runDir+File.separator+ number_of_initial_run +".data";
+            File fileInitialRun = new File(initial_run);
+            //initial run 생성하기
+            fileInitialRun.createNewFile();
+            dm.openDos(initial_run, blocksize);
+
+            for (MutableTriple<Integer, Integer, Integer> curData : dataArr) {
+                try{
+                    dm.writeNext(curData);
+                }catch (EOFException e) {
+                    break;
+                }
+            }
+
+            dm.closeDos();
+        }
         // File cursor 닫기
-        is.close();
-        os.close();
+        dm.closeDis();
+
+        // initial run 생성해서 quick sort 정렬까지 완료! 2) n-way merge만 구현하면 됨!
 
         // 2) n-way merge
 //        _externalMergeSort(tmpdir, outfile, 0);
@@ -139,20 +214,43 @@ public class TinySEExternalSort implements ExternalSort {
     class DataManager{
         public boolean isEOF = false;
         private DataInputStream dis = null;
+        private DataOutputStream dos = null;
         public MutableTriple<Integer, Integer, Integer> tuple = new MutableTriple<Integer, Integer, Integer>(0, 0, 0);
-        public DataManager(DataInputStream dis) throws IOException {
-
+        public DataManager() throws IOException {
         }
 
-        private boolean readNext() throws IOException {
-            if (isEOF) return false;
+        private void openDis(String path, int blocksize) throws FileNotFoundException {
+            this.dis = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(path), blocksize)
+            );
+        }
+
+        private void openDos(String path, int blocksize) throws FileNotFoundException {
+            this.dos = new DataOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(path), blocksize)
+            );
+        }
+
+        private void closeDis() throws IOException {
+            this.dis.close();
+        }
+
+        private void closeDos() throws IOException {
+            this.dos.close();
+        }
+
+        private void readNext() throws IOException {
             tuple.setLeft(dis.readInt()); tuple.setMiddle(dis.readInt()); tuple.setRight(dis.readInt());
-            return true;
         }
 
-        public void getTuple(MutableTriple<Integer, Integer, Integer> ret)throws IOException {
+        private void getTuple(MutableTriple<Integer, Integer, Integer> ret)throws IOException {
             ret.setLeft(tuple.getLeft()); ret.setMiddle(tuple.getMiddle()); ret.setRight(tuple.getRight());
-            isEOF = (! readNext());
+        }
+
+        private void writeNext(MutableTriple<Integer, Integer, Integer> ret) throws IOException {
+            dos.writeInt(ret.getLeft()); dos.writeInt(ret.getMiddle()); dos.writeInt(ret.getRight());
         }
     }
 }
